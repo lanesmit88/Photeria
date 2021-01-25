82a1c0af9c58dabe69a12e0a5e3c8043f9154592
giiaga <ghousali99@gmail.com>
2021-01-25 12:25:04 -0500

    Provider wrapped around app and window.store set to store

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import configureStore from "./store/storeLivesHere";
+import { Provider } from "react-redux";
 
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
