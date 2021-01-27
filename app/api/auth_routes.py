967862004d761d1c801953fd2ef0f31bb5bacf6c
mike <mikethecodegeek@gmail.com>
2021-01-27 14:15:35 -0800

    added route to get all comments for a post

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -40,6 +40,7 @@ def login():
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         # Add the user to the session, we are logged in!
+        print('Logged In')
         user = User.query.filter(User.email == form.data['email']).first()
         login_user(user)
         return user.to_dict()
--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import Post, PostLike
+from app.models import Post, PostLike, Comment
 
 post_routes = Blueprint('posts', __name__)
 
@@ -8,9 +8,19 @@ post_routes = Blueprint('posts', __name__)
 @post_routes.route('/<int:id>/likes')
 @login_required
 def postLikes(id):
-    post = PostLikes.query.filer(PostLikes.postId.equal(id)).all()
+    post = PostLikes.query.filter(PostLikes.postId.equal(id)).all()
     return {"users": [user.to_dict() for user in users]}
 # Get users another time
+
+
+@post_routes.route('/<int:id>/allcomments')
+def post_comments(id):
+    print(id)
+    comments = Comment.query.filter(Comment.postId==id).all()
+
+    return {"comments":[comment.to_dict() for comment in comments]}
+
+
 
 @post_routes.route('/<int:id>')
 @login_required
