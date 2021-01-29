382571fc368df16dbe7bf2ef3fa670988afc9ec8
giiaga <ghousali99@gmail.com>
2021-01-28 19:06:59 -0500

    got it to work

--- app/routes/messages.py
+++ app/routes/messages.py
@@ -8,12 +8,26 @@ bp = Blueprint('messages', __name__)
 def allMessagesFortheUser(id):
     # message = Message.query.get()
     # message = Message.query.()
-    user = User.query.get(int(id))
-    print(user)
-    recievedMessages = Message.query.filter(Message.recipientId == int(id)).order_by(Message.createdAt).all()
-    sentMessages = Message.query.filter(Message.senderId == int(id)).order_by(Message.createdAt).all()
+    # recievedMessages = Message.query.filter(Message.recipientId == int(id)).order_by(Message.createdAt).all()
+    # user = User.query.order_by(recievedMessages).get(int(id))
+    # print(user, 'HEEEEEEEEfgsdghsdfjkgjdfEEERE')
+    recievedMessag =  User.query.order_by(User.recievedMessages).get(int(id)).recievedMessages
+    recieveToDict = [message.to_dict() for message in recievedMessag]
+    # print(recieveToDict)
+    senderUsersFound = []
+    for item in recieveToDict:
+        test = User.query.get(item['senderId'])
+        senderUsersFound.append(test.to_dict())
+        # print(item['senderId'], 'GHDFSJGSDFGHSDFJH')
 
-    messages = {{'recievedMessages': [message.to_dict() for message in recievedMessages]},
-    {'sentMessages': [message.to_dict() for message in sentMessages]}}
+    # print('THIS IS THE LIUST', senderUsersFound)
+    # senderUsers = {'users': users.to_dict() for users in senderUsersFound}
+    # print('ghhfjgf', senderUsersFound)
+    # recMessagesToDict = {'recMessages': message.to_dict() for message in recievedMessages}
 
-    return jsonify([message.to_dict() for message in messages])
+    # sentMessages = Message.query.filter(Message.senderId == int(id)).order_by(Message.createdAt).all()
+
+    # messages = {{'recievedMessages': [message.to_dict() for message in recievedMessages]},
+    # {'sentMessages': [message.to_dict() for message in sentMessages]}}
+
+    return jsonify(recieveToDict, senderUsersFound)
