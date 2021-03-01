28c5d3d1f2876304c2f747fd295684bc3c18b172
Lane Smit <lanesmit88@outlook.com>
2021-03-01 13:50:59 -0600

    Trending component written out

--- react-app/src/components/Trending/index.js
+++ react-app/src/components/Trending/index.js
@@ -1,10 +1,27 @@
 import React from "react";
-
+import "./index.css";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchTrendingData } from "../../store/feed";
+import Post from "../Post";
+import { useEffect } from "react";
 function Trending() {
+  window.scrollTo(0, 0);
+  const dispatch = useDispatch();
+  const feed = useSelector((reduxState) => {
+    return reduxState.trending;
+  });
+  useEffect(() => {
+    dispatch(fetchTrendingData());
+  }, []);
+
   return (
-    <>
-      <h1>HI</h1>
-    </>
+    <div id="feedContainer">
+      <div id="feed-inner-container">
+        {feed.map((post) => {
+          return <Post key={post.id} post={post} />;
+        })}
+      </div>
+    </div>
   );
 }
 
